fba78bee2bf99cab55c1f636f58967ac560a6954
Maggie Correll <maggie.correll@gmail.com>
2016-08-29 18:28:34 -0400

    uncommented routers in urls

--- question-box/urls.py
+++ question-box/urls.py
@@ -3,16 +3,17 @@ from rest_framework import routers
 from question import views
 from django.contrib import admin
 
-# router = routers.DefaultRouter()
-# router.register(r'question', views.QuestionViewSet)
-# router.register(r'answer', views.AnswerViewSet)
-# router.register(r'comment', views.CommentViewSet)
-# router.register(r'tag', views.TagViewSet)
+router = routers.DefaultRouter()
+router.register(r'question', views.QuestionViewSet)
+router.register(r'answer', views.AnswerViewSet)
+router.register(r'comment', views.CommentViewSet)
+router.register(r'tag', views.TagViewSet)
+router.register(r'user', views.UserProfileViewSet)
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('question.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # url(r'^api/', include(router.urls))
+    url(r'^api/', include(router.urls))
 ]
