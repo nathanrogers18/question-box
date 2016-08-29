718aa8fa528691fad439062aa66683ed11b16e42
Nathan <nathanrogers18@gmail.com>
2016-08-29 17:14:26 -0400

    Updated views and settings

--- question/views.py
+++ question/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import * # TODO: Change this to the specific models
 
 # Create your views here.
 def index(request):
@@ -10,11 +10,10 @@ def index(request):
 
 
 def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
+    # greeting = Greeting()
+    # greeting.save()
+    #
+    # greetings = Greeting.objects.all()
+    #
+    # return render(request, 'db.html', {'greetings': greetings})
+    return render(request, 'db.html', {'context': 'context'})
