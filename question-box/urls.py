bcdeb1d71ba0ba1adea04cc89a705af5cd22a880
Nathan <nathanrogers18@gmail.com>
2016-08-29 17:36:38 -0400

    Updated urls

--- question-box/urls.py
+++ question-box/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import question.views
 
 urlpatterns = [
     url(r'^$', question.views.index, name='index'),
