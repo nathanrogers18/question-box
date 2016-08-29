09999116e8b6244b974b5a86e0e621242862ed71
Nathan <nathanrogers18@gmail.com>
2016-08-29 17:09:26 -0400

    Modified database

--- question-box/settings.py
+++ question-box/settings.py
@@ -64,11 +64,11 @@ WSGI_APPLICATION = 'question-box.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {}
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
