48348eb6f958eb012c44e475cb16d3e835a07691
Tara Davis <tkalindavis@gmail.com>
2016-08-31 17:03:18 -0400

    Working on all templates

--- question/views.py
+++ question/views.py
@@ -81,8 +81,9 @@ class UserProfileDetail(generic.DetailView):
     template_name = 'profile_detail.html'
     context_object_name = 'profile'
 
-    def get_context_data(self, **kwargs):
-        context = super(UserProfileDetail, self).get_context_data(**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserProfileDetail, self).get_context_data(*args, **kwargs)
+        context['username'] = User.objects.all()
         return context
 
 
